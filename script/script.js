aca0de7808dd4ea5bb10778252eb464cb7a2d331
artem <bakirovartem69@gmail.com>
2022-07-14 15:29:33 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,39 @@
+/*let score = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+let score1 = (5 + 5) * 10;
+console.log("Ergebnis: " + score1);
+
+let score2 = 0;
+score2 = score2 + 10;
+console.log("Ergebnis: " + score2);
+
+score2 += 10;
+console.log("Ergebnis: " + score2);*/
+
+/*let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+
+zahl++;
+
+console.log("increment", zahl);
+
+zahl--;
+
+console.log("increment", zahl);*/
+
+let addition_operator = 1 + 1;
+console.log("addition: " + addition_operator);
+
+let subtraktion_operator = 2 - 1;
+console.log("subtraktion: " + subtraktion_operator);
+
+let multiplication_operator = 2 * 2;
+console.log("multiplication: " + multiplication_operator);
+
+let division_operator = 4 / 2;
+console.log("division: " + division_operator);
+
+let modulus_operator = 14 % 5;
+console.log("modulus: " + modulus_operator);
